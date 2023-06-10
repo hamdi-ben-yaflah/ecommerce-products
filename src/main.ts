af968a837b69753f89a4138b176aa1e367121df4
hamdi-ben-yaflah <benyaflahhamdi2@gmail.com>
2023-06-10 11:33:19 +0200

    set origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: (origin, callback) => {
+      callback(null, origin && origin.startsWith('http://localhost'));
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
